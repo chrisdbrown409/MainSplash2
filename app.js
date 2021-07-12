6d645a09e711a4503bd5a7349a285bf75516832b
chrisdbrown409 <85533707+chrisdbrown409@users.noreply.github.com>
2021-07-12 16:19:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 	const splash = document.queryselector('.splash');
 
-	document.addeventlistener('DomContentLoaded',(e)=>{
+	document.addeventlistener('DomContentLoaded', (e)=>{
 		setTimeout (()=>{
 			splash.classlist.add('display-none');
 		}, 2000),
